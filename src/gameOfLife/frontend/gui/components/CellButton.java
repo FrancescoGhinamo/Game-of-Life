fcb1e3c84304a021265abff63f122dec8c9f1ad2
FrancescoGhinamo <44404163+FrancescoGhinamo@users.noreply.github.com>
2019-04-08 21:12:05 +0200

    Created CellButton

--- src/gameOfLife/frontend/gui/components/CellButton.java
+++ src/gameOfLife/frontend/gui/components/CellButton.java
@@ -1,15 +1,23 @@
 package gameOfLife.frontend.gui.components;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.util.Observable;
+import java.util.Observer;
 
 import javax.swing.JButton;
 
-public class CellButton extends JButton {
+import gameOfLife.backend.beam.Cell;
+import gameOfLife.backend.beam.CellsBoard;
+
+@SuppressWarnings("deprecation")
+public class CellButton extends JButton implements Observer {
 
 	private static final long serialVersionUID = -2241712941561906943L;
 	
 	private int x, y;
-	
+	private Cell linkedCell;
 	
 	
 	
@@ -17,12 +25,39 @@ public class CellButton extends JButton {
 		super();
 		this.x = x;
 		this.y = y;
+		CellsBoard.getInstance().getBoard()[x][y].addObserver(this);
+		this.linkedCell = CellsBoard.getInstance().getBoard()[x][y];
+		this.setSize(new Dimension(5, 5));
 	}
 
 
 
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
+
+	@Override
+	public void update(Observable o, Object arg) {
+		if(this.linkedCell.isAlive()) {
+			this.setBackground(Color.WHITE);
+		}
+		else {
+			this.setBackground(Color.BLACK);
+		}
+		
 	}
 
+
+
+
+	public int getX() {
+		return x;
+	}
+
+
+
+
+	public int getY() {
+		return y;
+	}
+
+	
+	
 }
